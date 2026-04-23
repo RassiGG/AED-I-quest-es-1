5e93a0d699d615e15b87c92f0fd0ec8cd0089165
RASSI <leonardoali2008@gmail.com>
2026-04-23 09:16:50 -0400

    parte 1

---
+++ alfabetomaemi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int m;
+    int k;
+    char alfabetoMaiusculo[26];
+    char alfabetoMinusculo[26];
+    for(int m = 'A'; m <= 'Z'; m++){
+         alfabetoMaiusculo[m];
+
+} 
+for(int k = 'a'; k<= 'z'; k++){
+    alfabetoMinusculo[k];
+
+}
+printf("Alfabetos Maiusculos:", "%i", m);
+printf("Alfabetos Minusculos:", "%i", k );
+
+
+
+
+
+    return 0;
+}
